fa8693c5c70bc7a31b846f89c721165bcd088538
jonathan <infixz520@gmail.com>
2015-09-26 23:00:10 +0800

    framework

---
+++ server/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"这个文件将承载所有的设定"
+
+# IP addr
+server_addr = "42.96.204.227" # vps name = "chengsvps"
+# service port
+webpage_port = 80
+ws_port = 8080
---
+++ server/server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import tornado.httpserver
+import tornado.httpclient
+import tornado.web
+import tornado.options
+import tornado.websocket as ws
+import tornado.ioloop
+
